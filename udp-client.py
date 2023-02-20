a099ce3209b0943167782617c7de907cf5bc8590
mrasamny <mrasamny@desu.edu>
2023-02-20 15:34:12 -0500

    implementation completed

--- udp-client.py
+++ udp-client.py
@@ -1,12 +1,13 @@
 import socket
 
-server_address = '10.33.16.111'
+server_address = '127.0.0.1'
 port = 13000
 
 if __name__ == "__main__":
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     message = input("What's your message: ")
-    client_socket.sendto(message.encode(), (server_address, port))
+    size = len(message).to_bytes(4, byteorder='big')
+    client_socket.sendto(size + message.encode(), (server_address, port))
     response, address = client_socket.recvfrom(1024)
     print(f'Response from {address[0]} is "{response.decode()}"')
     client_socket.close()
--- udp-server.py
+++ udp-server.py
@@ -1,6 +1,6 @@
 import socket
 
-my_address = '10.33.16.111'
+my_address = '127.0.0.1'
 port = 13000
 
 if __name__ == "__main__":
@@ -10,8 +10,14 @@ if __name__ == "__main__":
     try:
         while True:
             message, client_address = server_socket.recvfrom(1024)
+            size = message[:4]
+            message = message[4:]
+            size = int.from_bytes(size, byteorder='big')
+            print(f'size = {size} and message = {message}')
+            # message, client_address = server_socket.recvfrom(size)
             message = message.decode()
             response = ''.join(list(map(lambda ch: '' if ch in 'aeiou' else ch, message)))
+            print(message+":"+response)
             server_socket.sendto(response.encode(), client_address)
     except KeyboardInterrupt as ki:
         print("Shutting down...")
